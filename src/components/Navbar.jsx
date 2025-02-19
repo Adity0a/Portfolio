dc7e5d20a89251c37794c3fee25fefa79d6bf552
Adity0a <159717774+Adity0a@users.noreply.github.com>
2025-02-19 13:34:17 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import pic from "../../public/Aditya Mishra.png";
+import pic from "../../public/photo.avif";
 import { AiOutlineMenu } from "react-icons/ai";
 import { IoCloseSharp } from "react-icons/io5";
 import { Link } from "react-scroll";
@@ -32,7 +32,7 @@ function Navbar() {
       <div className="max-w-screen-2xl container mx-auto px-4 md:px-20 h-16 shadow-md fixed top-0 left-0 right-0 z-50 bg-white">
         <div className="flex justify-between items-center h-16">
           <div className=" flex space-x-2">
-            <img src="/Aditya Mishra.png" className="h-12 w-12 rounded-full" alt="" />
+            <img src="/photo.avif" className="h-12 w-12 rounded-full" alt="" />
             <h1 className="font-semibold text-xl cursor-pointer">
               <span className="text-green-500 text-2xl">A</span>ditya
               <p className="text-sm">Web Developer</p>
